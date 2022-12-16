648c80e4d20973908ef21afebc9dc3793bc36860
James Shaw <jamesshaw112@sky.com>
2022-12-16 17:25:54 +0000

    Remove package.lock and prettier

--- src/day10/Day10.ts
+++ src/day10/Day10.ts
@@ -1,13 +1,13 @@
-import { readFromFile, splitLines } from '../utils';
+import { readFromFile, splitLines } from "../utils";
 
 export class Day10 {
   private stringDataIn = "";
 
-  constructor(){
+  constructor() {
     readFromFile("day10").then((data) => {
       this.stringDataIn = data;
       this.mainImpl();
-    })
+    });
   }
 
   private mainImpl() {
@@ -25,68 +25,72 @@ export class Day10 {
     let lineN = 0;
     let count = 40;
 
-    while(true){
+    while (true) {
       cycle++;
-      console.log("=== CYCLE ", cycle ," START ===");
-      if(count === 0){
+      console.log("=== CYCLE ", cycle, " START ===");
+      if (count === 0) {
         lineN++;
         count = 40;
       }
 
-      if(!screenLine[lineN]){
+      if (!screenLine[lineN]) {
         screenLine.push("");
       }
 
       console.log("Current CRT row ", screenLine[lineN]);
-      
 
-      if(cycle === 20 || cycle === 60 || cycle === 100 || cycle === 140 || cycle === 180 || cycle === 220){
+      if (
+        cycle === 20 ||
+        cycle === 60 ||
+        cycle === 100 ||
+        cycle === 140 ||
+        cycle === 180 ||
+        cycle === 220
+      ) {
         signalStrength += cycle * X;
       }
 
       let commands = [];
 
-      if(lineToRead === lines.length){
+      if (lineToRead === lines.length) {
         commands[0] = "finish";
-      }
-      else{
+      } else {
         commands = lines[lineToRead].split(" ");
       }
 
       console.log(commands);
 
       console.log("X = ", X);
-      
-      if(commands[0] === "noop"){
+
+      if (commands[0] === "noop") {
         lineToRead++;
-      }
-      else if(toExecute === "addx"){
-          toExecute = "";
-          X += parseInt(commands[1]);
-          lineToRead++;
-      }
-      else if(commands[0] === "addx"){
-          toExecute = "addx";
+      } else if (toExecute === "addx") {
+        toExecute = "";
+        X += parseInt(commands[1]);
+        lineToRead++;
+      } else if (commands[0] === "addx") {
+        toExecute = "addx";
       }
 
       //console.log("Current pos", screenLine[lineN].length);
-      if(screenLine[lineN].length + 1 === X || screenLine[lineN].length + 1 === X-1 || screenLine[lineN].length + 1 === X+1){
+      if (
+        screenLine[lineN].length + 1 === X ||
+        screenLine[lineN].length + 1 === X - 1 ||
+        screenLine[lineN].length + 1 === X + 1
+      ) {
         screenLine[lineN] += "#";
-      }
-      else{
+      } else {
         screenLine[lineN] += ".";
       }
 
-      if(commands[0] === "finish"){
+      if (commands[0] === "finish") {
         break;
       }
       count--;
     }
 
     console.log("Answer 1: ", signalStrength);
-  
 
     console.log(screenLine);
-    
   }
 }
